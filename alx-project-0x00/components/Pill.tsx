d51013cebd57784de0ca7a0b0d5a58ff9ee51e4b
DevOsi123 <gatelogistics@consultant.com>
2025-05-30 03:41:18 -0700

    added I new image in the card.tsx component folder

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,15 +1,10 @@
-import React from "react";
 
-interface PillProps {
-  title: string;
-}
-
-const Pill: React.FC<PillProps> = ({ title }) => {
+const Pill: React.FC = () => {
   return (
-    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">{title}</p>
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
     </div>
-  );
-};
+  )
+}
 
 export default Pill;
